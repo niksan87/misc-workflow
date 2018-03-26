5eb243e6501a603e8d1e2815369fb27436ef8907
nikolay.stefanov <nikolay.stefanov@skywindgroup.com>
2018-03-26 12:52:15 +0300

    added mustache gallery

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,14 @@
-global.jQuery = require('jquery');
 bootstrap = require('bootstrap');
 mustache = require('mustache');
 
+
+
 jQuery(document).ready(function(){
-	alert('hello');
+	let jqxhr = $.get("data.json", {data: "value"}, function(json) {
+		console.log(json.gallery);
+		let template = $('#template').html();
+		console.log(template);
+		let showTemplate = mustache.render(template, json);
+		$('#gallery').html(showTemplate);
+	}, "json")
 });
